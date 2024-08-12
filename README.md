51059e7873c0346fe46fe6434ca423b40821990c
Saloni <tester1.elitetechno@gmail.com>
2024-08-12 12:46:18 +0530

    Home_page

--- README.md
+++ README.md
@@ -54,3 +54,4 @@
 # Strike_Automation
 # Strike_Automation
 # Strike_Automation
+# Strike_Automation
--- cypress/e2e/Backtesting_Option/Banknifty_Cash.cy.js
+++ cypress/e2e/Backtesting_Option/Banknifty_Cash.cy.js
@@ -568,7 +568,7 @@ it("Option Backtesting", () => {
 
   cy.get('[style="float: none;"] > .common_anchor').click();
 
-  cy.get(".text-start > .full_wrap > .form-control").type("Teststrategies123");
+  cy.get(".text-start > .full_wrap > .form-control").type("Teststrategies689");
 
   /*
   let basename = "Test";
