92f67b8cb6ad91c13967e689cfb156cb4fc0035c
Saloni <tester1.elitetechno@gmail.com>
2024-08-30 10:20:48 +0530

    Home_page

--- cypress/e2e/Backtesting_Option/Banknifty_Cash.cy.js
+++ cypress/e2e/Backtesting_Option/Banknifty_Cash.cy.js
@@ -568,7 +568,7 @@ it("Option Backtesting", () => {
 
   cy.get('[style="float: none;"] > .common_anchor').click();
 
-  cy.get(".text-start > .full_wrap > .form-control").type("users902");
+  cy.get(".text-start > .full_wrap > .form-control").type("users1002");
 
   /*
   let basename = "Test";
